987e049518a36b9fd1b49b105de529f3e10c787d
ajay-singh-rajput <ajay091.ar@gmail.com>
2023-11-01 09:38:06 +0530

    publish HUYU

---
+++ script.js
@@ -0,0 +1,50 @@
+// var a = 50;
+// if(a%2 === 0){
+//     console.log("Number Is Even");
+// }
+
+// var b = prompt("Input your age");
+
+// if(b > 17){
+//     alert(" your are eligible for voting")
+// } else {
+//     alert(" your are not eligible for voting")
+
+// }
+
+// ledder if else
+// var a = prompt("enter any munber");
+
+// if(a<0){
+//     console.log('number is negative')
+// } else if(a>0){
+//     console.log('number is positive')
+// } else{
+//     console.log('number is zero')
+// }
+
+
+// var a = 12;
+// var b = 55;
+// var c = 23;
+
+// if (a > b && a > c){
+// console.log('a is greater');
+// } else if (b > a && b > c){
+// console.log('b is greater');
+
+// }else if (c > a && c > b){
+// console.log('c is greater');
+// } else {
+// console.log('all are equal');
+// }
+
+// console.log("grater in two digit");
+
+// if(a>b){
+// console.log('a is greater');
+// } else if (b>a){
+// console.log('b is greater');
+// } else {
+//     console.log('both are equal');
+//     }
